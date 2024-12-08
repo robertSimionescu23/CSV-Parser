5f2035b16fb37ddade2659c16243581099c00806
robertSimionescu23 <simionescurobert23@gmail.com>
2024-12-08 12:36:40 +0200

    Changed structure to compile both banks statements intro respective folders, without specifying whcih bank

--- bankStatementParser.py
+++ bankStatementParser.py
@@ -30,13 +30,10 @@ def monthStringToNumber(month): #Turn the name of a month into it's correspondin
         case "decembrie":
             return ("12")
 
-
 transactionTypesEnum = ("Incasare ", "Cumparare", "Transfer ")
 
-
-def parseStatements(bankName, filePath):
-    checkInNRows = -1
-    transactionText = ""
+def parseINGCSVFile(file):
+    statementFilesPath = "./Statements/ING/"
 
     #Info to be extracted
     transactionDate   = ""
@@ -47,64 +44,88 @@ def parseStatements(bankName, filePath):
     #Full transaction list
     transcationList = []
 
-    if(bankName == "ING"):
-        with open(filePath, newline = '') as csvfile:
-            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    checkInNRows = -1 # -1 is normal functioning, no future vendor info sampling planned.
 
-            for row in reader:
-                row = list(filter(None, row)) #Remove all empty elements
+    filePath = statementFilesPath + file
 
-                if(checkInNRows != -1): #Get the vendor type in N rows
-                    checkInNRows -= 1
+    with open(filePath, newline = '') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
 
-                #For easier reading
-                secondLastElement = row[len(row) - 2]
-                LastElement       = row[len(row) - 1]
+        for row in reader:
+            row = list(filter(None, row)) #Remove all empty elements
 
-                if(secondLastElement[0] == "\""): #Sums are between " ", but divided in 2 elements. Bring them together, and remove quatation marks and decimal point.
-                    secondLastElement = secondLastElement.replace(".","") #Remove the decimal point as it is uneeded.
-                    secondLastElement = ".".join([secondLastElement, LastElement]) #Join elements.
-                    secondLastElement = secondLastElement.replace("\"","") #remove quatation.
+            if(checkInNRows != -1): #Get the vendor type in N rows
+                checkInNRows -= 1
 
-                    row[len(row) - 2] = secondLastElement
-                    transcationValue = secondLastElement #Add the value to the info list
-                    row.pop(len(row) - 1) #Remove last element, as elements are joined
+            #For easier reading
+            secondLastElement = row[len(row) - 2]
+            LastElement       = row[len(row) - 1]
 
-                if(checkInNRows == 0):
-                    if(row[0][0 : 9] != "Referinta"): #Case that deviates from the usual. Need to check next row to get the vendor.
-                        transactionVendor = row[0]
-                        checkInNRows = -1
-                        ### Adding the value to the transaction list ###
-                        transcationList.append([transactionDate, transcationType, transcationValue, transactionVendor])
-                    else:
-                        checkInNRows = 1
+            if(secondLastElement[0] == "\""): #Sums are between " " characters, but divided in 2 elements. Bring them together, and remove quatation marks and decimal point.
+                secondLastElement = secondLastElement.replace(".","") #Remove the decimal point as it is uneeded.
+                secondLastElement = ".".join([secondLastElement, LastElement]) #Join elements.
+                secondLastElement = secondLastElement.replace("\"","") #remove quatation.
 
-                #If the first character of the csv row is a date without "-", it is the first line of the transaction info.
-                if(row[0][0] in str(list(range(0,4))) and row[0][2] != "-"):
-                    transactionDate = row[0]           #This is the date in a word format
-                    transcationType = row[1] #This is the type, "incasare", "transfer", "cumparare"
-                    if(row[1] == "Incasare"): #If the type matches, get the vendor type in N rows
-                        checkInNRows = 1
-                    elif(row[1] == "Transfer Home'Bank"):
-                        checkInNRows = 2
-                    elif(row[1] == "Cumparare POS"):
-                        checkInNRows = 2
+                row[len(row) - 2] = secondLastElement
+                transcationValue = secondLastElement #Add the value to the info list
+                row.pop(len(row) - 1) #Remove last element, as elements are joined
 
-        #After reading through the csv file, raw data will be saved to the transcationList list. It will need to be processed.
-        #  --- Processing the data ---  #
+            if(checkInNRows == 0):
+                if(row[0][0 : 9] != "Referinta"): #Case that deviates from the usual. Need to check next row to get the vendor.
+                    transactionVendor = row[0]
+                    checkInNRows = -1
+                    ### Adding the value to the transaction list ###
+                    transcationList.append([transactionDate, transcationType, transcationValue, transactionVendor])
+                else:
+                    checkInNRows = 1
 
-        for transaction in transcationList:
-            #  --- Date ---   #
-            date = transaction[0].split()
-            month = date[1]
-            year  = date[2]
+            #If the first character of the csv row is a date without "-", it is the first line of the transaction info.
+            if(row[0][0] in str(list(range(0,4))) and row[0][2] != "-"):
+                transactionDate = row[0]           #This is the date in a word format
+                transcationType = row[1] #This is the type, "incasare", "transfer", "cumparare"
+                if(row[1] == "Incasare"): #If the type matches, get the vendor type in N rows
+                    checkInNRows = 1
+                elif(row[1] == "Transfer Home'Bank"):
+                    checkInNRows = 2
+                elif(row[1] == "Cumparare POS"):
+                    checkInNRows = 2
 
+    return transcationList
+
+def parseINGStatements():
+    statementFilesPath = "./Statements/ING/"
+    monthreport = [] # A list that will contain, the month, year and transaction of said month
+    for file in os.listdir(statementFilesPath): #Iterate to every statement file (CSV) in the ING reports
+        transactionList = parseINGCSVFile(file)
+        date = transactionList[0][0].split(" ") #We can get the month and year from the first element, as it is always the same format
+        month = date[1]
+        year  = date[2]
+        monthreport.append([year, month, transactionList])
+
+    return monthreport
+
+
+
+def parseStatements():
+
+    transactionText = ""
+
+    monthlyReportsING = parseINGStatements()
+
+    #  --- Processing the data ---  #
+    for entry in monthlyReportsING:
+
+        year  = entry[0]
+        month = entry[1]
+        print(entry[2])
+        for transaction in entry[2]:
+        # Formatting the data
+            # --- Date --- #
             transaction[0] = transaction[0].replace(month, monthStringToNumber(month)) # e.g. november --> 11
 
             transaction[0] = transaction[0].replace(" ", "/")
 
             #  --- Type ---  #
-
             match(transaction[1]):
                 case("Incasare"):
                     transaction[1] = transactionTypesEnum[0]
@@ -113,8 +134,7 @@ def parseStatements(bankName, filePath):
                 case("Transfer Home'Bank"):
                     transaction[1] = transactionTypesEnum[2]
 
-            #  --- Value --- #
-
+            #  --- Value ---
             if(len(transaction[2]) < 7):
                 for i in range (7 - len(transaction[2])):
                     transaction[2] += " "
@@ -163,12 +183,4 @@ def parseStatements(bankName, filePath):
     #     f.close()
 
 if __name__ == "__main__":
-    if(len(sys.argv) == 2):
-        if(sys.argv[1] == "ING" or sys.argv[1] == "Revolut"):
-            bankStatementFolderPath = "./Statements/" + sys.argv[1]
-            for statementFile in os.listdir(bankStatementFolderPath):
-                parseStatements(sys.argv[1], bankStatementFolderPath + "/" + statementFile)
-        else:
-            print("Not a valid command")
-    else:
-        print(" Provide a Bank (ING or Revolut) \n or \n Request a general report (add report at the end of command)")
+    parseStatements()
